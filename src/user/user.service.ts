00282771e560c71386db595f85f0fee16a07888d
quyendch11 <quyendch11@gmail.com>
2022-11-10 21:43:28 +0700

    user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,27 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user';
 import { User } from './user.entity';
-
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User) 
-    private userRepo: Repository<User>) { }
-  async createUser(body: CreateUserDto) {
-    try{
-      let user = await this.userRepo.save(body);
-      return user;
-    }catch(err){
-      console.log(err);
-      
-    }
+    private userRepo: Repository<User>,
+    private jwtService:JwtService,
+    ) {}
+  async register(body: CreateUserDto):Promise<CreateUserDto> {
+    const hashPassword = this.hashPassword(body.password)
+    await this.userRepo.save({...body,password:hashPassword});
+    return body;
    
+  }
+  async login(username: string, password: string) {
+    const user = await this.findOneByUsername(username);
+    if (user) {
+      if (await this.comparePassword(password, user.password)) {
+        return user;
+      }
+    }
+    throw new UnauthorizedException();
+  }
+
+  async generateToken (payload:User){
+    return this.jwtService.sign({
+      id:payload.id,
+    })
+
+  }
+
+  async hashPassword(password:string): Promise<any>{
+    return await bcrypt.hash(password,12);
+  }
+  async comparePassword(
+    password: string,
+    hashPassword: string,
+  ): Promise<boolean> {
+    return await bcrypt.compare(password, hashPassword);
   }
  async findAll() {
    return await this.userRepo.find(); 
  }
+ async findOneByUsername(username: string): Promise<User> {
+  return await this.userRepo.findOne({ where: { username } });
+}
  async findById(id:number){
    let user = await this.userRepo.findOne({
      where: {id},
